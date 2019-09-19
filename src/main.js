dd3f40edb2019589e6ceb90d0cfdc680d537eee7
18252030586 <z923891099@icloud.com>
2019-09-19 15:06:21 +0800

    use vue-lazyload

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import MintUI from 'mint-ui';
+import VueLazyload from 'vue-lazyload';
 import 'mint-ui/lib/style.css';
 import App from './App';
 import router from './router';
@@ -11,6 +12,9 @@ import './assets/css/index.scss';
 
 Vue.config.productionTip = false;
 Vue.use(MintUI);
+Vue.use(VueLazyload, {
+  loading: require('./assets/img/default.png')
+});
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/views/study/imageLoad.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="(item,index) in imgList" :key="index">
+      <img class="item-pic" v-lazy="item.url"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'imageLoad',
+    data() {
+      return {
+        imgList: [
+          { url: require('./../../../static/img/card-1.jpg') },
+          { url: require('./../../../static/img/card-2.jpg') },
+          { url: require('./../../../static/img/card-3.jpg') },
+          { url: require('./../../../static/img/card-1.jpg') },
+          { url: require('./../../../static/img/card-2.jpg') },
+          { url: require('./../../../static/img/card-3.jpg') }
+        ]
+      };
+    }
+  };
+</script>
+
+<style scoped>
+  .item-pic {
+    width: 100%;
+  }
+</style>
